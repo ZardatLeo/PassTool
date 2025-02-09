3d404b8c1ab8f48630c5009b81cdce871fdb1284
zardatleo <z2402519120@gmail.com>
2025-02-09 16:54:18 +0800

    PassTool

--- correct_test.py
+++ correct_test.py
@@ -1,6 +1,15 @@
 from tqdm import tqdm
+import argparse
 
-filename = 'result.txt' # 生成的口令文件
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gen_filename', type=str, default='result.txt', help='生成的口令文件', required=True)
+    parser.add_argument('--test_filename', type=str, default='passlen8_test.txt', help='测试集文件', required=True)
+    return parser.parse_args()
+
+args = parse_args()
+
+filename = args.gen_filename # 生成的口令文件
 pass_set = set()
 with open(filename, 'r', encoding = 'utf-8') as file:
     pwds = file.readlines()
@@ -8,7 +17,7 @@ with open(filename, 'r', encoding = 'utf-8') as file:
         pass_set.add(pwds[i])
 
 test_set = {}
-test_filename = "passlen8_test.txt"      #测试集文件
+test_filename = args.test_filename #测试集文件
 with open(test_filename, 'r', encoding = 'utf-8') as file:
     test_pwds = file.readlines()
     for i in tqdm(range(len(test_pwds)), desc = '正在处理测试集...'): #去掉换行符
@@ -18,20 +27,16 @@ print('生成口令重复率:', (len(pwds) - len(pass_set)) / len(pwds) * 100, '
 print('有效猜测次数:', len(pass_set))
 correct = 0
 cnt = 0
-node = [1e5, 1e6, 1e7, 1e8]
-for p in tqdm(pass_set, desc = '进度'):
+node = [1e6, 1e7, 1e8]
+nd_cnt = 0
+for p in pass_set:
     cnt += 1
     if p in test_set:
         correct += 1
-    if cnt == 1e5:
-        print('1e5准确数:', correct)
-        print('1e5准确率:', min(1, correct / len(test_set)) * 100, '%')
-    if cnt == 1e6:
-        print('1e6准确数:', correct)
-        print('1e6准确率:', min(1, correct / len(test_set)) * 100, '%')
-    if cnt == 1e7:
-        print('1e7准确数:', correct)
-        print('1e7准确率:', min(1, correct / len(test_set)) * 100, '%')
+    if cnt == node[nd_cnt]:
+        nd_cnt += 1
+        print(f'{node[nd_cnt - 1]}准确数:', correct)
+        print(f'{node[nd_cnt - 1]}准确率:', min(1, correct / len(test_set)) * 100, '%')
 
 print('最终准确数:', correct)
 print('最终准确率:', min(1, correct / len(test_set)) * 100, '%')
